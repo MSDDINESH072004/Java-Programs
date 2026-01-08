aa8e5e3bad44dba7451840ed7e5ca1498dadb404
MSDDINESH072004 <149169666+MSDDINESH072004@users.noreply.github.com>
2026-01-08 14:29:56 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {15, 32, 45, 54, 60}; // sorted
+        int target = 54;
+        int start = 0;
+        int end = arr.length - 1;
+        int foundIndex = -1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == target) {
+                foundIndex = mid;
+                break;
+            }
+            if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        if (foundIndex != -1) {
+            System.out.println("Found at index: " + foundIndex);
+        } else {
+            System.out.println("Not found");
+        }
+    }
+}
