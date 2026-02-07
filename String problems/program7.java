d1f6f51712b793eaceb5a5a64b2ca6f42b927b07
Dinesh T <149169666+MSDDINESH072004@users.noreply.github.com>
2026-02-07 12:25:29 +0530

    Create program7.java

---
+++ String problems/program7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class program7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        String copy = "";
+        for (int i = 0; i < s.length(); i++)
+            copy += s.charAt(i);
+
+        System.out.println(copy);
+    }
+}
